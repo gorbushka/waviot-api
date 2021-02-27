f209ec852edcda3175b99e009276f043868de794
Semikin <is.semikin@gmail.com>
2021-02-27 12:24:34 +0300

    add class

---
+++ waviot.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import requests
+import json
+
+class Waviot:
+    login_url='https://auth.waviot.ru/?action=user-login'
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def get_login(self):
+        WAVIOT_JWT=False
+        payload = {"login":self.login,"password":self.password}
+        header = {'Content-type': 'application/json','X-requested-with': 'XMLHttpRequest'}
+        r=requests.post(self.login_url,data=json.dumps(payload),headers=header)
+        if r.status_code==200:
+            try:
+                WAVIOT_JWT=json.loads(r.text)['WAVIOT_JWT']
+                print(WAVIOT_JWT)
+                self.WAVIOT_JWT=WAVIOT_JWT
+            except:
+                print("no WAVIOT_JWT")
+        else:
+            print(r.status_code)
+            print("something goes wrong",r.headers)
+        #return WAVIOT_JWT
+
+    def get_request(self,url):
+        header = {'Content-type': 'application/json',
+                  'X-requested-with': 'XMLHttpRequest',
+                  'Authorization': 'bearer ' + self.WAVIOT_JWT}
+        r=requests.get(url,headers=header)
+        if r.status_code==200:
+            try:
+                response=json.loads(r.text)
+                self.response=response
+            except:
+                print(f"no response {r.text}")
+        else:
+            print("error",r.status_code)
+            print("something goes wrong",r.headers)
+        return response
+   
+    def parse_response_roll(self):
+        for modem in self.response:
+            print(modem)
+            for record in self.response[modem]:
+                print("NEW RECORD\n {} \n===========\n".format(record))
+                try:
+                    print("timestamp: {}, snr: {}, rssi: {}".format(
+                          record['timestamp'],
+                          record['snr'],record['rssi']))
+                except:
+                    print("no")
+                try:
+                    for sensor in record['data'].values():
+                        print("sensor:{},value:{}".format(sensor['name'],sensor['value']))
+                    print("\n==========\n")
+                except:
+                    print ("no")
+        return True
